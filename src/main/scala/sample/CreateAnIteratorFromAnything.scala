57b3f5d238e1c3f389a1a018a2d689721d296e6f
Francois-Xavier Bonnet <francois-xavier.bonnet@centraliens.net>
2018-10-11 16:44:12 +1100

    Iterators

---
+++ src/main/scala/sample/CreateAnIteratorFromAnything.scala
@@ -0,0 +1,27 @@
+package sample
+
+object CreateAnIteratorFromAnything extends App {
+
+  /**
+    * Returns a limited number of Strings and then null. There is no method to check if there is a next entry or not.
+    */
+  class SomeNonStandardIterableThing {
+    private var count = 0
+
+    def next: String = {
+      count = count + 1
+      if (count > 5)
+        null
+      else
+        s"Element # $count"
+    }
+  }
+
+  val source = new SomeNonStandardIterableThing
+
+  // Convert our object to a standard scala Iterator
+  val iterator = Iterator.continually(Option(source.next)).takeWhile(_.isDefined).flatten
+
+  // Now we can use it in a for comprehension \o/
+  for (str <- iterator) println(str)
+}
